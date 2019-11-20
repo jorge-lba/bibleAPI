c7effef070586271afb2badedf6b4a888c9ae1f5
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2019-11-20 18:26:41 -0300

    Implementando e testando metodo get HTTP

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 // Import Holy Bible Content
 const Bible = require('./controller/bible')
 
+//
+const port = 3003;
+
+const express = require('express');
+const app = express();
+
+const bodyParser = require('./node_modules/body-parser');
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/bible',(req, res, next) =>{
+    const bibleV = new Bible(
+        req.body.language,
+        req.body.version
+        ).get(
+            req.body.book,
+            req.body.chapter,
+            req.body.verse
+            )
+    res.send(bibleV); 
+})
+
 // Select language and version
 const getLanguage = 'pt-br';
 const getVersion = 'NVI'
@@ -20,3 +42,7 @@ const [book, chapter, verse, title] = bible.get(getBook,getChapter,getVerse)[1]
 // Next pass is control the data to use and view
 
 console.log(JSON.stringify(bibleValue),chapter, verse, title)
+
+app.listen(port, ()=>{      //Iniciando porta
+    console.log(`Servidor executando na porta ${port}`);
+});
