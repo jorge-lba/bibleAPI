87db564dd9f78419f9eb4755fcb98ae05782b04d
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2019-11-21 01:04:38 -0300

    Implementando Firebase, o req body não está retornando os parametro da url

---
+++ public/index.js
@@ -0,0 +1,53 @@
+// Import Holy Bible Content
+const Bible = require('./controller/bible')
+
+//
+const port = 8080;
+
+const express = require('express');
+const app = express();
+
+const bodyParser = require('./node_modules/body-parser');
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/bible',(req, res, next) =>{
+    const bibleV = new Bible(
+        req.body.language,
+        req.body.version
+        ).get(
+            req.body.book,
+            req.body.chapter,
+            req.body.verse
+            )
+        
+    console.log( req.body.language, req.body.book,
+        req.body.chapter,
+        req.body.verse,
+        req.body.version)
+    res.send(bibleV); 
+})
+
+// Select language and version
+const getLanguage = 'pt-br';
+const getVersion = 'NVI'
+
+// Instancie new class(Holy Bible)
+const bible = new Bible(getLanguage, getVersion);
+
+// Set values for get content
+const getBook = 'mateus';
+const getChapter = 1;
+const getVerse = 1;
+
+const bibleValue = bible.get(getBook,getChapter,getVerse)[0]
+const [book, chapter, verse, title] = bible.get(getBook,getChapter,getVerse)[1]
+
+
+// Next pass is control the data to use and view
+
+console.log(JSON.stringify(bibleValue),chapter, verse, title)
+
+app.listen(port, ()=>{      //Iniciando porta
+    console.log(`Servidor executando na porta ${port}`);
+});
